9da0d13ea550dae08db5b455bc89c90595ea1682
Ban Vuong <vuongdieuban@gmail.com>
2018-11-17 12:24:10 -0700

    backend: static and media files

--- backend/FEH/settings.py
+++ backend/FEH/settings.py
@@ -116,9 +116,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = "/static/"
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "backend")
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIR = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+# client upload to backend/media
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 
 # Rest Framework
